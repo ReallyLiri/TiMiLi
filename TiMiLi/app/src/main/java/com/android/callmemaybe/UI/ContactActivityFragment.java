3bfc667bc77bd52f57f49bfbe43523f1233e1798
Tiana <tiana_l@live.com>
2016-03-09 23:38:49 +0200

    continued with the buttonAction class added to the fav and block listeners in contactActivity

--- TiMiLi/app/src/main/java/com/android/callmemaybe/UI/ContactActivityFragment.java
+++ TiMiLi/app/src/main/java/com/android/callmemaybe/UI/ContactActivityFragment.java
@@ -1,5 +1,6 @@
 package com.android.callmemaybe.UI;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.callmemaybe.UI.data.ButtonAction;
 import com.android.callmemaybe.UI.data.Contact;
 import com.android.callmemaybe.UI.databinding.ContactFragmentBinding;
 import com.android.callmemaybe.helpers.ContactHelper;
@@ -36,6 +38,7 @@ public class ContactActivityFragment extends Fragment {
 
         String phoneNumber = getActivity().getIntent().getStringExtra(ContactActivity.PHONE_NUMBER_EXTRA);
         mContact = ContactHelper.getContact(phoneNumber);
+        final ButtonAction buttonAction = new ButtonAction(mContact);
 
         if (mContact == null) {
             Toast.makeText(getContext(), "Failed to get contact for number: " + phoneNumber, Toast.LENGTH_LONG).show();
@@ -50,10 +53,12 @@ public class ContactActivityFragment extends Fragment {
         Button callBtn = binding.contactFragmentCallBtn;
         callBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                String uri = "tel:" + mContact.getPhoneNumber();
+                buttonAction.callAction(getContext());
+
+                /*String uri = "tel:" + mContact.getPhoneNumber();
                 Intent intent = new Intent(Intent.ACTION_DIAL);
                 intent.setData(Uri.parse(uri));
-                startActivity(intent);
+                startActivity(intent);*/
             }
         }) ;
 
@@ -61,11 +66,13 @@ public class ContactActivityFragment extends Fragment {
         smsBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String uri = "tel:" + mContact.getPhoneNumber();
+                buttonAction.msgAction(getContext());
+
+                /*String uri = "tel:" + mContact.getPhoneNumber();
                 Intent intent = new Intent(Intent.ACTION_SENDTO);
                 intent.setData(Uri.parse(uri));
                 Intent chooser = Intent.createChooser(intent, "Send message with...");
-                startActivity(chooser);
+                startActivity(chooser);*/
             }
         });
 
@@ -74,7 +81,21 @@ public class ContactActivityFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
+                Context context = getContext();
+                buttonAction.blockAction(context, v);
+                if (getActivity().isTaskRoot()) {
+                    //the app's backstack is empty, so start MainActivity
+                    Intent goToMainActivity = new Intent(context, MainActivity.class);
+                    //the flag delets from stack's all activities comint after the target activity
+                    goToMainActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    context.startActivity(goToMainActivity);
 
+                } else {
+                    //TODO: go to previous activity. we cant use finishActivity() because its a fragment
+                    getActivity().finish();
+                    //not sure whether to use onBackPressed, onDestroy or finish
+                    //still need to delete from backstack
+                }
             }
         });
 
--- TiMiLi/app/src/main/java/com/android/callmemaybe/UI/data/ButtonAction.java
+++ TiMiLi/app/src/main/java/com/android/callmemaybe/UI/data/ButtonAction.java
@@ -1,6 +1,15 @@
 package com.android.callmemaybe.UI.data;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.view.View;
+import android.widget.Toast;
+
 
 /**
  * Created by Ana on 09/03/2016.
@@ -12,15 +21,53 @@ public class ButtonAction {
         contact = currContact;
     }
 
-    public void favAction (View v) {
+    public void favAction (Context context, View v) {
         contact.toggleFavorite();
         //change picture
     }
 
-    public void blockAction (View v) {
+    public void blockAction (final Context context, View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
 
+        builder.setTitle("Confirm");
+        builder.setMessage("Are you sure you want to block this user?");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO: update server that this user is blocked
+                Toast.makeText(context, "this user is blocked!!", Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        AlertDialog alert = builder.create();
+        alert.show();
 
     }
 
+    public void msgAction (Context context) {
+        String uri = "tel:" + contact.getPhoneNumber();
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse(uri));
+        Intent chooser = Intent.createChooser(intent, "Send message with...");
+        context.startActivity(chooser);
+    }
+
+    public void callAction (Context context) {
+        String uri = "tel:" + contact.getPhoneNumber();
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(uri));
+        context.startActivity(intent);
+    }
 
 }
